1ffbf8166a49d8b92ce02db3a91dedb0cddc4453
Omeljan Konstantin <kos.omeljan@gmail.com>
2022-02-06 15:15:35 +0200

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,16 +1,17 @@
 # change encoding to utf-8
+import time
 f = open('text.txt', 'r')
 list = []
 
 for i in f:
-	'''list.append(i)'''
 	a = i.split(';')
 	if a[0] in list:
-		pass
+		print('This quote is already in the list')
 	else:
 		list.append(a[0])
+		print('Add value to list - ' + a[0])
 		z = open('kotir.txt', 'a')
 		z.write(a[0] + '\n')
 		z.close()
 
-print(list)
+print("Things are good")
